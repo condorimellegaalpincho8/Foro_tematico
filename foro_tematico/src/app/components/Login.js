efe2af26b283a383e2f89da724f9ba370a6a2ce7
unknown <sarmientogonzalesdavid@gmail.com>
2024-11-09 23:46:07 -0500

    primer commit

---
+++ foro_tematico/src/app/components/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica de autenticación
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Iniciar sesión</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">Usuario:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Contraseña:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ foro_tematico/src/app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Foro</h1>
+    </div>
+  );
+}
